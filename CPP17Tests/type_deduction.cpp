b2f67615326d0d8bd8947aa7719514c8b144818b
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-04 17:04:00 +0200

    extended item 1 with array & function cases

--- CPP17Tests/type_deduction.cpp
+++ CPP17Tests/type_deduction.cpp
@@ -30,6 +30,11 @@ constexpr void deduce_type_pass_by_value(T param) {
 	cout << "\tdecltype(param) = " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
 };
 
+template<typename T, size_t N>
+constexpr size_t size_of_array(T(&)[N]) noexcept {
+	return N;
+}
+
 void type_deduction() {
 
 	// item 1:
@@ -40,6 +45,8 @@ void type_deduction() {
 	const int& cir = ci;
 	int* ip = &i;
 	const int* cip = &ci;
+	const char ccstr[5] = "Test";
+	void func(int);
 
 	cout << "reminder: In the following paragraph the constness refers allways to the top level constness of the mentioned type. So in this manner `const int*` is non-const!" << endl << endl;
 	// case 1a:
@@ -58,6 +65,10 @@ void type_deduction() {
 	deduce_type_reference(ip);
 	cout << "const int* -->" << endl;
 	deduce_type_reference(cip);
+	cout << "const char[] -->" << endl;
+	deduce_type_reference(ccstr);
+	cout << "void ()(int) -->" << endl;
+	deduce_type_reference(func);
 	cout << endl;
 	// case 1b:
 	cout << "item_1.case_1b: template type deduction via pointer" << endl;
@@ -68,6 +79,10 @@ void type_deduction() {
 	deduce_type_pointer(ip);
 	cout << "const int* -->" << endl;
 	deduce_type_pointer(cip);
+	cout << "const char[] -->" << endl;
+	deduce_type_pointer(ccstr);
+	cout << "void ()(int) -->" << endl;
+	deduce_type_pointer(func);
 	cout << endl << endl;
 
 	// case 2a:
@@ -86,6 +101,10 @@ void type_deduction() {
 	deduce_type_universal_reference(ip);
 	cout << "lvlaue const int* -->" << endl;
 	deduce_type_universal_reference(cip);
+	cout << "lvalue const char[] -->" << endl;
+	deduce_type_universal_reference(ccstr);
+	cout << "lvalue void ()(int) -->" << endl;
+	deduce_type_universal_reference(func);
 	cout << endl;
 	// case 2b:
 	cout << "item_1.case_2b: template type deduction via universal reference called with rvalues" << endl;
@@ -103,6 +122,10 @@ void type_deduction() {
 	deduce_type_universal_reference(move(ip));
 	cout << "rvlaue const int* -->" << endl;
 	deduce_type_universal_reference(move(cip));
+	cout << "rvalue const char[] -->" << endl;
+	deduce_type_universal_reference(move(ccstr));
+	cout << "rvalue void ()(int) -->" << endl;
+	deduce_type_universal_reference(move(func));
 	cout << endl << endl;
 
 	// case 3:
@@ -122,10 +145,19 @@ void type_deduction() {
 	deduce_type_pass_by_value(ip);
 	cout << "const int* -->" << endl;
 	deduce_type_pass_by_value(cip);
+	cout << "const char[] -->" << endl;
+	deduce_type_pass_by_value(ccstr);
+	cout << "void ()(int) -->" << endl;
+	deduce_type_pass_by_value(func);
 	cout << endl << endl;
 
+	// built-in array ref
+	int array[3];
+	cout << "The array has " << size_of_array(array) << " elements" << endl << endl;
 
-	// item 4: get type names from the compiler
+
+	// item 4:
+	// get type names from the compiler
 	// set-up variables
 	auto x = ci;
 	auto y = &ci;
@@ -139,3 +171,5 @@ void type_deduction() {
 	cout << "decltype(y) = " << type_id_with_cvr<decltype(y)>().pretty_name() << endl;
 	cout << endl;
 }
+
+void func(int) {}
