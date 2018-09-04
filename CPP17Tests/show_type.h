fbd7c2954649d8b2023dcebd577756fe3404a2e5
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-04 16:18:48 +0200

    minimized compiler error message for show_type

--- CPP17Tests/show_type.h
+++ CPP17Tests/show_type.h
@@ -4,5 +4,4 @@ template<typename T>
 class type_displayer;
 
 // Use this function to get a compiler error containing information about the type of x.
-template<typename T>
-constexpr auto show_type(T x) { return type_displayer<decltype(x)>{}; }
+#define show_type(x) type_displayer<decltype(x)>{}
