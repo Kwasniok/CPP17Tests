fca601884e72e819668024c7e794074fa2fb0bd3
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-04 16:29:38 +0200

    added item1

--- CPP17Tests/type_deduction.cpp
+++ CPP17Tests/type_deduction.cpp
@@ -4,8 +4,126 @@
 
 using namespace std;
 using namespace boost::typeindex;
+
+// functions for item 1
+template<typename T>
+constexpr void deduce_type_reference(T& param) {
+	cout << "\t              T = " << type_id_with_cvr<T>().pretty_name() << endl;
+	cout << "\tdecltype(param) = " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+template<typename T>
+constexpr void deduce_type_pointer(T* param) {
+	cout << "\t              T = " << type_id_with_cvr<T>().pretty_name() << endl;
+	cout << "\tdecltype(param) = " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+template<typename T>
+constexpr void deduce_type_universal_reference(T&& param) {
+	cout << "\t              T = " << type_id_with_cvr<T>().pretty_name() << endl;
+	cout << "\tdecltype(param) = " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+template<typename T>
+constexpr void deduce_type_pass_by_value(T param) {
+	cout << "\t              T = " << type_id_with_cvr<T>().pretty_name() << endl;
+	cout << "\tdecltype(param) = " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
 void type_deduction() {
-	const int ci = 7;
+
+	// item 1:
+	// template type deduction
+	int i = 3;
+	const int ci = i;
+	int& ir = i;
+	const int& cir = ci;
+	int* ip = &i;
+	const int* cip = &ci;
+
+	cout << "reminder: In the following paragraph the constness refers allways to the top level constness of the mentioned type. So in this manner `const int*` is non-const!" << endl << endl;
+	// case 1a:
+	cout << "item_1.case_1a: template type deduction via reference" << endl;
+	cout << "summary: T is the const-preserved non-ref type " << endl << 
+		    "         & declatype(param) is T&." << endl;
+	cout << "int -->" << endl;
+	deduce_type_reference(i);
+	cout << "const int -->" << endl;
+	deduce_type_reference(ci);
+	cout << "int& -->" << endl;
+	deduce_type_reference(ir);
+	cout << "const int& -->" << endl;
+	deduce_type_reference(cir);
+	cout << "int* -->" << endl;
+	deduce_type_reference(ip);
+	cout << "const int* -->" << endl;
+	deduce_type_reference(cip);
+	cout << endl;
+	// case 1b:
+	cout << "item_1.case_1b: template type deduction via pointer" << endl;
+	cout << "summary: T is the non-pointer type " << endl <<
+		"         & declatype(param) is T*." << endl <<
+		"reminder: Pointer types only!;" << endl;
+	cout << "int* -->" << endl;
+	deduce_type_pointer(ip);
+	cout << "const int* -->" << endl;
+	deduce_type_pointer(cip);
+	cout << endl << endl;
+
+	// case 2a:
+	cout << "item_1.case_2a: template type deduction via universal reference called with lvalues" << endl;
+	cout << "summary: T is the const-preserved refernece type " << endl << 
+		"         & declatype(param) is T." << endl;
+	cout << "lvalue int -->" << endl;
+	deduce_type_universal_reference(i);
+	cout << "lvalue const int -->" << endl;
+	deduce_type_universal_reference(ci);
+	cout << "lvalue int& -->" << endl;
+	deduce_type_universal_reference(ir);
+	cout << "lvalue const int& -->" << endl;
+	deduce_type_universal_reference(cir);
+	cout << "lvalue int* -->" << endl;
+	deduce_type_universal_reference(ip);
+	cout << "lvlaue const int* -->" << endl;
+	deduce_type_universal_reference(cip);
+	cout << endl;
+	// case 2b:
+	cout << "item_1.case_2b: template type deduction via universal reference called with rvalues" << endl;
+	cout << "summary: T is the const-preserved non-refernece type " << endl << 
+	        "         & declatype(param) is T&&." << endl;
+	cout << "rvalue int -->" << endl;
+	deduce_type_universal_reference(move(i));
+	cout << "rvalue const int -->" << endl;
+	deduce_type_universal_reference(move(ci));
+	cout << "rvalue int& -->" << endl;
+	deduce_type_universal_reference(move(ir));
+	cout << "rvalue const int& -->" << endl;
+	deduce_type_universal_reference(move(cir));
+	cout << "rvalue int* -->" << endl;
+	deduce_type_universal_reference(move(ip));
+	cout << "rvlaue const int* -->" << endl;
+	deduce_type_universal_reference(move(cip));
+	cout << endl << endl;
+
+	// case 3:
+	cout << "item_1.case_3: template type deduction via pass by value" << endl;
+	cout << "summary: T is the non-const non-refernece type (basic type)" << endl << 
+		    "         & declatype(param) is T." << endl << 
+		    "remember: int * and const int * are basic types!" << endl;
+	cout << "int -->" << endl;
+	deduce_type_pass_by_value(i);
+	cout << "const int -->" << endl;
+	deduce_type_pass_by_value(ci);
+	cout << "int& -->" << endl;
+	deduce_type_pass_by_value(ir);
+	cout << "const int& -->" << endl;
+	deduce_type_pass_by_value(cir);
+	cout << "int* -->" << endl;
+	deduce_type_pass_by_value(ip);
+	cout << "const int* -->" << endl;
+	deduce_type_pass_by_value(cip);
+	cout << endl << endl;
+
 
 	// item 4: get type names from the compiler
 	// set-up variables
