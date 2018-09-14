ef0e7bc5bba9751e8d530d0cf9371a64bef0ee15
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-14 04:50:24 +0200

    added solution

--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -1,4 +1,30 @@
 ﻿
 #include <iostream>
+#include <cassert>
+#include <string>
 
-int main() { }
+struct ci_char_traits: public std::char_traits<char> {
+	static bool compare(const char * const lhs, const char * const rhs, const size_t n) {
+		return _memicmp(lhs, rhs, n);
+	}
+	static bool eq(char lhs, char rhs) {
+		return tolower(lhs) == tolower(rhs);
+	}
+	static bool lt(char lhs, char rhs) {
+		return tolower(lhs) < tolower(rhs);
+	}
+	static const char* find(const char* s, int n, char a) {
+		while (n-- > 0 && toupper(*s) != toupper(a)) { ++s; }
+		return n > 0 ? s : 0;
+	}
+};
+
+using ci_string = std::basic_string<char, ci_char_traits, std::allocator<char>>;
+
+int main() {
+	ci_string s("AbCdE");
+	assert( s == "abcde" );
+	assert( s == "ABCDE" );
+	assert( strcmp( s.c_str(), "AbCdE" ) == 0 );
+	assert( strcmp( s.c_str(), "abcde" ) != 0 );
+}
