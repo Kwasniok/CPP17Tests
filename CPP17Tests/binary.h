5afcab8b95d68a8e48b719ec4964c0979e4af372
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-14 03:12:29 +0200

    added chapter001 binary

---
+++ CPP17Tests/binary.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+template <unsigned long N>    struct binary {
+	static unsigned const value = binary<N/10>::value << 1 | N%10;
+};
+
+template <>
+struct binary<0>
+{ static unsigned const value = 0; };
+
+unsigned const one = binary<1>::value;
+unsigned const three = binary<11>::value;
+unsigned const five = binary<101>::value;
+unsigned const seven = binary<111>::value;
+unsigned const nine = binary<1001>::value;
--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -1,6 +1,5 @@
-
+﻿
 #include <iostream>
+#include "binary.h"
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-}
+int main() { }
