2a7f6da36dc0b6cf075cf46bba8fa5df6276f1d1
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-15 17:11:15 +0200

    generalized to insentitive string class

--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -3,7 +3,16 @@
 #include <cassert>
 #include <string>
 
-struct ci_char_traits: public std::char_traits<char> {
+template <int v>
+constexpr char to_std_repr(char c) {
+	// converts characters to thier standard representation
+	// e.g. for case intensitivity 'A' and 'a' are mapped to 'a' etc. (lowercase ist standard representation)
+	// default is identity
+	return c;
+}
+
+template<int v>
+struct insensitive_char_traits: public std::char_traits<char> {
 	static constexpr bool compare(const char * lhs, const char * rhs, size_t n) {
 		// adapted from: http://www.cplusplus.com/reference/string/char_traits/compare/
 		// could be optimized
@@ -11,31 +20,57 @@ struct ci_char_traits: public std::char_traits<char> {
 		return 0;
 	}
 	static constexpr bool eq(char lhs, char rhs) {
-		return to_std_repr(lhs) == to_std_repr(rhs);
+		return to_std_repr<v>(lhs) == to_std_repr<v>(rhs);
 	}
 	static constexpr bool lt(char lhs, char rhs) {
-		return to_std_repr(lhs) < to_std_repr(rhs);
+		return to_std_repr<v>(lhs) < to_std_repr<v>(rhs);
 	}
 	static constexpr const char* find(const char* s, int n, char a) {
-		while (n-- > 0 && to_std_repr(*s) != to_std_repr(a)) { ++s; }
-		return n > 0 ? s : 0;
-	}
-	static constexpr char to_std_repr(const char c) {
-		// override for insensitivity
-		// converts A-Z to a-z (assuming ASCII)
-		// A := 0x41, Z := 0x5A
-		// a := 0x61, z := 0x7A
-		if (0x41 <= c && c <= 0x5A) return c + 0x20;
-		return c;
+		while (n-- > 0 && to_std_repr<v>(*s) != to_std_repr<v>(a)) { ++s; }
+		return n > 0 ? s : nullptr;
 	}
 };
 
-using ci_string = std::basic_string<char, ci_char_traits, std::allocator<char>>;
+constexpr int NONE = 0;
+constexpr int CASE = 1;
+constexpr int DIGIT = 2;
+
+template<>
+constexpr char to_std_repr<CASE>(char c) {
+	// override for insensitivity of cases
+	// converts A-Z to a-z (assuming ASCII)
+	// A := 0x41, Z := 0x5A
+	// a := 0x61, z := 0x7A
+	if (0x41 <= c && c <= 0x5A) return c + 0x20;
+	return c;
+}
+
+template<>
+constexpr char to_std_repr<DIGIT>(const char c) {
+	// override for insensitivity of digits
+	// converts 0-9 to 0 (assuming ASCII)
+	// 0 := 0x30, 9 := 0x39
+	if (0x30 <= c && c <= 0x39) return 0x30;
+	return c;
+}
+
+// define insensitive string classes
+using ci_string = std::basic_string<char, insensitive_char_traits<CASE>, std::allocator<char>>;
+using di_string = std::basic_string<char, insensitive_char_traits<DIGIT>, std::allocator<char>>;
 
 int main() {
-	ci_string s("AbCdE");
-	assert( s == "abcde" );
-	assert( s == "ABCDE" );
-	assert( strcmp( s.c_str(), "AbCdE" ) == 0 );
-	assert( strcmp( s.c_str(), "abcde" ) != 0 );
+	{
+		ci_string s("AbCdE");
+		assert(s == "abcde");
+		assert(s == "ABCDE");
+		assert(strcmp(s.c_str(), "AbCdE") == 0);
+		assert(strcmp(s.c_str(), "abcde") != 0);
+	}
+	{
+		di_string s("Hello 12");
+		assert(s == "Hello 89");
+		assert(s == "Hello 26");
+		assert(strcmp(s.c_str(), "Hello 12") == 0);
+		assert(strcmp(s.c_str(), "Hello 00") != 0);
+	}
 }
