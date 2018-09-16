d6e63f765b8cbb759f0cf3623bfb88ce9752997a
Kwasniok <Kwasniok@users.noreply.github.com>
2018-09-16 21:52:38 +0200

    first approach

--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -1,4 +1,21 @@
 ﻿
 #include <iostream>
 
-int main() { }
+template<typename T>
+class Stack {
+public:
+	Stack() { };
+	~Stack() {
+		delete[] mem_;
+		mem_ = nullptr;
+	}
+
+private:
+	T* mem_ = nullptr;
+	size_t size_ = 0;
+	size_t capacity_ = 0;
+};
+
+int main() {
+	Stack<int> s;
+}
