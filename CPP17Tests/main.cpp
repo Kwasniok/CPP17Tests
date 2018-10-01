71b76a51bb584530ba7f5800cf3bde8781b34dd1
Kwasniok <Kwasniok@users.noreply.github.com>
2018-10-01 23:07:09 +0200

    adapted solution

--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -13,7 +13,6 @@ protected:
 	StackImpl( const StackImpl& ) = delete;
 	~StackImpl();
 	StackImpl& operator=( const StackImpl& ) = delete;
-	void extend(const size_t n);
 	void swap(StackImpl& rhs) noexcept;
 	bool valid() const noexcept { return mem_; }
 protected:
@@ -46,30 +45,6 @@ void StackImpl<T>::swap(StackImpl<T>& rhs) noexcept
 	std::swap(size_, rhs.size_);
 	std::swap(capacity_, rhs.capacity_);
 }
-template<typename T>
-void StackImpl<T>::extend(const size_t n)
-{
-	// try allocation a new block of memory
-	T* tmp = static_cast<T*> (operator new(sizeof(T)*(capacity_ + n)));
-	// try copying
-	try {
-		copy(mem_, mem_+size_, tmp);
-	} catch (...) {
-		// failure: clean up!
-		operator delete(tmp);
-		// behave exception-neutral
-		throw;
-	}
-	// copy was successful: changes can now be applied to the program
-	// use nothrow fuctions from this point on!
-	// destruct all old used elemetnts & deallocate the old memory
-	//if(mem_!=nullptr) { ... <-- possible optimization
-	destroy(mem_, mem_+size_); // nullptr-safe
-	operator delete (mem_); // nullptr-safe if std implementation
-	// apply changes
-	mem_ = tmp;
-	capacity_ += n;;
-}
 
 class empty_stack_error: public std::runtime_error
 {
@@ -83,15 +58,16 @@ template<typename T>
 class Stack : private StackImpl<T>
 {
 public:
-	static constexpr size_t default_capacity_chunk_size = 8;
+	static constexpr size_t default_initial_capacity = 8;
+	static constexpr size_t default_extension_capacity = 8;
 public:
 	typedef T* iterator;
 	typedef const T* const_iterator;
 public:
-	Stack(size_t capacity = default_capacity_chunk_size) : StackImpl<T>(capacity) { }
-	Stack(const Stack& rhs);
+	Stack(size_t capacity = default_initial_capacity) : StackImpl<T>(capacity) { }
+	Stack(const Stack& rhs, size_t extra_capacity=0);
 	Stack& operator=(const Stack& rhs);
-	~Stack() noexcept { }
+	~Stack() = default;
 public:
 	void push(const T& v);
 	T& top() const throw(empty_stack_error);
@@ -106,34 +82,45 @@ public:
 };
 
 template<typename T>
-Stack<T>::Stack(const Stack& rhs) : StackImpl<T>(rhs.size_)
+Stack<T>::Stack(const Stack& rhs, size_t extra_capacity) : StackImpl<T>(rhs.size_ + extra_capacity)
 {
 	// if the super constructor failes the destructor of the base class is called and hence the memory is deallocated
 	// try copying
-	copy(rhs.begin(), rhs.end(), this->mem_);
+	while (this->size_ < rhs.size_)
+	{
+		new(this->mem_ + this->size_) T(rhs.mem_[this->size_]);
+		++this->size_;
+	}
+	// if an exception occurs the destructor of the base class is called and all constructedelements are destroyed
+	// and the memory is deallocated
 }
 template<typename T>
 Stack<T>& Stack<T>::operator=(const Stack& rhs)
 {
-	if (this != &rhs)
-	{
-		// create a copy of rhs
-		Stack<T> tmp(rhs);
-		// success!
-		this->swap(tmp); // noexcept
-	}
+	// create a copy of rhs
+	Stack<T> tmp(rhs);
+	// success: swap with this instance
+	this->swap(tmp); // noexcept
 	return *this;
+	// tmp (containing the old values) is implicitly destructed
 }
 
 template<typename T>
 void Stack<T>::push(const T& v)
 {
 	// appends the new element at the end
-	// extend memory if needed
-	if (size() == capacity()) this->extend(default_capacity_chunk_size);
-	// since the memory slot ist not initialized the new element is constructed in place
-	new(this->mem_ + this->size_) T(v);
-	++this->size_;
+	// extend memory if needed and push back the new element
+	if (this->size_ == this->capacity_)
+	{
+		Stack tmp(*this, default_extension_capacity);
+		tmp.push(v);
+		this->swap(tmp);
+	} else {
+		// no new memory is needed and the new element is appended in uninitialized memory
+		// since the memory slot ist not initialized the new element is constructed in place
+		new(this->mem_ + this->size_) T(v);
+		++this->size_;
+	}
 }
 
 template<typename T>
@@ -151,10 +138,9 @@ void Stack<T>::pop() throw(empty_stack_error)
 	// remove last element
 	// an empty stack throws an empty_stack_error
 	if (empty()) throw empty_stack_error("empty_stack_error: cannot pop from empty Stack");
-	// release resources of original (nothrow)
-	this->mem_[this->size_ - 1].~T();
-	// update stack state
+	// destroy last element (nothrow)
 	--this->size_;
+	this->mem_[this->size_].~T();
 }
 
 template<typename T>
