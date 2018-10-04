f149046cbe55e7e382eb72d73f2e3193390d9e84
Kwasniok <Kwasniok@users.noreply.github.com>
2018-10-04 16:05:49 +0200

    initial try

--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -1,4 +1,32 @@
 ﻿
 #include <iostream>
+#include <string>
 
-int main() { }
+using namespace std;
+
+struct Employee
+{
+	const string first_ = "Ben";
+	const string last_ = "Parker";
+	const string title_ = "CEO";
+	const string& first() { return first_; }
+	const string& last() { return last_; }
+	const string& title() { return last_; }
+	int salary() { return 10; }
+};
+
+string evaluate(Employee& e)
+{
+	string name = e.first() + " " + e.last();
+	if (e.title() == "CEO" || e.salary() > 5)
+	{
+		string msg = name + "is overpaid\n"; // not portable!
+		cout << msg;
+	}
+	return name; // noexcept move
+}
+
+int main() {
+	Employee e;
+	string s = evaluate(e);
+}
