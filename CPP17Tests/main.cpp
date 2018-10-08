474e1d5491a0f068c9564b5c1a21377a8d8c908a
Kwasniok <Kwasniok@users.noreply.github.com>
2018-10-08 16:22:40 +0200

    extended for private inheritance

--- CPP17Tests/main.cpp
+++ CPP17Tests/main.cpp
@@ -5,32 +5,56 @@
 
 using namespace std;
 
+
 struct Base
 {
 	void f(int) { cout << "Base::f(int)"; }
 };
-
-struct Derived_without_using: public Base
+// case 1: public inheritance
+struct Derived_public_without_using: public Base
 {
-	void f(double) { cout << "Derived_without_using::f(double)"; }
+	void f(double) { cout << "Derived_public_without_using::f(double)"; }
 };
-
-struct Derived_with_using: public Base
+struct Derived_public_with_using: public Base
 {
 	using Base::f;
-	void f(double) { cout << "Derived_with_using::f(double)"; }
+	void f(double) { cout << "Derived_public_with_using::f(double)"; }
+};
+// case 2: private inheritance
+struct Derived_private_without_using: private Base
+{
+	void f(double) { cout << "Derived_private_without_using::f(double)"; }
+};
+struct Derived_private_with_using: private Base
+{
+	using Base::f;
+	void f(double) { cout << "Derived_private_with_using::f(double)"; }
 };
 
 int main()
 {
 	{
-		do_verbose(Derived_without_using d);
+		do_verbose(Derived_public_without_using d);
 		do_verbose(d.f(1));
 		do_verbose(d.f(1.0));
 	}
+	cout << endl;
 	{
-		do_verbose(Derived_with_using d);
+		do_verbose(Derived_public_with_using d);
 		do_verbose(d.f(1));
 		do_verbose(d.f(1.0));
 	}
+	cout << endl << endl;
+	{
+		do_verbose(Derived_private_without_using d);
+		do_verbose(d.f(1));
+		do_verbose(d.f(1.0));
+	}
+	cout << endl;
+	{
+		do_verbose(Derived_private_with_using d);
+		do_verbose(d.f(1));
+		do_verbose(d.f(1.0));
+	}
+	cout << endl;
 }
